4ee27d409428d465a14fe49feac6402305652d58
Victoria García <112613403+victoriagaarcia@users.noreply.github.com>
2024-02-22 22:37:19 +0100

    Python file that sends data to Kafka (producer)

--- mandar_datos.py
+++ mandar_datos.py
@@ -1,23 +1,15 @@
 from kafka import KafkaProducer
 import time
 import random 
-from datetime import datetime
+from datetime import datetime, date
 import json
-from realtime import DataCollectionRealTime
+from DataCollection import DataCollection
 
-
-# data_collection = DataCollectionRealTime()
-# empresas, ciks = data_collection.obtener_empresas('Real Estate')
-# datos = data_collection.extraer_datos(empresas, ciks, 2024)
-# data_collection.guardar_datos(datos)
-
+data_collection = DataCollection()
+empresas, ciks = data_collection.obtener_empresas('Real Estate')
+datos = data_collection.extraer_datos(empresas, ciks, 2024, "2024-01-01", date.today())
+data = datos.to_dict(orient="records")
 producer = KafkaProducer(bootstrap_servers='192.168.80.34:9092')
-
-# leer el archivo json y convertirlo en una lista de diccionarios
-with open('data_realtime.json') as file:
-    data = json.load(file)
-    
-# enviar cada diccionario como un mensaje al topic 'test'
 for record in data:
     producer.send('real_estate', json.dumps(record).encode('utf-8'))
     time.sleep(0.001)
